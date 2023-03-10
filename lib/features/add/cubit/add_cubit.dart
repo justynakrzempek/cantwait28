a4361a210e0f3a60f12349e7bd2482e6c5cbb96c
Justyna Krzempek <justyna.zakodowana@gmail.com>
2023-03-10 17:21:17 +0100

    Show snackbar on adding error

--- lib/features/add/cubit/add_cubit.dart
+++ lib/features/add/cubit/add_cubit.dart
@@ -8,8 +8,13 @@ part 'add_state.dart';
 class AddCubit extends Cubit<AddState> {
   AddCubit() : super(const AddState());
 
-  Future<void> add(String title, String imageURL, DateTime releaseDate,) async {
+  Future<void> add(
+    String title,
+    String imageURL,
+    DateTime releaseDate,
+  ) async {
     try {
+      // throw Exception('Coś poszło nie tak'); //tak można wywołać error, żeby sprawdzić czy prawidłowo się wyświetla, poniższe dane 18-24 zakomentowane
       await FirebaseFirestore.instance.collection('items').add(
         {
           'title': title,
