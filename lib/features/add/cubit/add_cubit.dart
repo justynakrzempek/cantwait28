c3f0e13e99f993d2c0c172b2f3ab89b2e9bfa316
Justyna Krzempek <justyna.zakodowana@gmail.com>
2023-03-16 11:46:19 +0100

    Separate logic to itemsRepository

--- lib/features/add/cubit/add_cubit.dart
+++ lib/features/add/cubit/add_cubit.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cantwait28/repositories/items_repository.dart';
 
 part 'add_state.dart';
 
 class AddCubit extends Cubit<AddState> {
-  AddCubit() : super(const AddState());
+  AddCubit(this._itemsRepository) : super(const AddState());
+
+  final ItemsRepository _itemsRepository;
 
   Future<void> add(
     String title,
@@ -15,13 +17,8 @@ class AddCubit extends Cubit<AddState> {
   ) async {
     try {
       // throw Exception('Coś poszło nie tak'); //tak można wywołać error, żeby sprawdzić czy prawidłowo się wyświetla, poniższe dane 18-24 zakomentowane
-      await FirebaseFirestore.instance.collection('items').add(
-        {
-          'title': title,
-          'image_url': imageURL,
-          'release_date': releaseDate,
-        },
-      );
+      await _itemsRepository.add(title, imageURL, releaseDate);
+
       emit(const AddState(saved: true));
     } catch (error) {
       emit(AddState(errorMessage: error.toString()));
